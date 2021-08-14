4baeeeae8df31e319b8fc44514fcd375eaf96c4f
Logan Merazzi <logan.merazzi@gavb.com.br>
2021-08-14 20:03:13 -0300

    Ajuste no nome do arquivos para o deploy

--- infra/s3_files.tf
+++ infra/s3_files.tf
@@ -2,6 +2,6 @@ resource "aws_s3_bucket_object" "job_spark" {
     bucket = aws_s3_bucket.dl.id
     key = "emr-code/pyspark/job_spark_from_tf.py"
     acl = "private"
-    source = "../Projetos/job_pyspark.py"
-    etag = filemd5("../Projetos/job_pyspark.py")
+    source = "../Projetos/job_spark.py"
+    etag = filemd5("../Projetos/job_spark.py")
 }
