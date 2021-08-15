07aacf08d8847fc1c5c914a1363fdd630d144372
Logan Merazzi <logan.merazzi@gavb.com.br>
2021-08-15 20:31:31 -0300

    Novos arquivos para o desafio

--- infra/s3_files.tf
+++ infra/s3_files.tf
@@ -5,3 +5,20 @@ resource "aws_s3_bucket_object" "job_spark" {
     source = "../Projetos/job_spark.py"
     etag = filemd5("../Projetos/job_spark.py")
 }
+
+resource "aws_s3_bucket_object" "delta_insert" {
+    bucket = aws_s3_bucket.dl.id
+    key = "emr-code/pyspark/01_delta_spark_insert.py"
+    acl = "private"
+    source = "../Projetos/etl/01_delta_spark_insert.py"
+    etag = filemd5("../Projetos/etl/01_delta_spark_insert.py")
+}
+
+resource "aws_s3_bucket_object" "delta_upsert" {
+    bucket = aws_s3_bucket.dl.id
+    key = "emr-code/pyspark/02_delta_spark_upsert.py"
+    acl = "private"
+    source = "../Projetos/etl/02_delta_spark_upsert.py"
+    etag = filemd5("../Projetos/etl/02_delta_spark_upsert.py")
+}
+
