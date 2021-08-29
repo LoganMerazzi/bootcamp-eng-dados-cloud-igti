6fcd42bf39fedffe6414bc94e2542b4c9516ad8e
Logan D Merazzi <x503219@gruposantander.com>
2021-08-29 14:40:38 -0300

    Alterações para validar a criação dos novos recursos

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# bootcamp-eng-dados-cloud-igti
+### Bootcamp de **Engenheiro de Dados Cloud** @ **IGTI**
+
+## Objetivo do Exercício:
+Entregar, através de uma esteira de Integração Contínua pelo GitHub e IaC via Terraform, um pipeline de dados completo.
+Itens que devem ser criados:
+ - Bucket no S3
+ - Carregar alguns arquivos
+ - Envio de uma função Lambda
+ - Permissões e acessos dentro do S3
+
+## Observações:
+- Todas as anotações realizadas durante as aulas encontram-se no arquivo Anotacoes.txt (Aham, eu tenho uma capacidade de dar nomes originais que você não imagina...)
+
+
--- infra/s3.tf
+++ infra/s3.tf
@@ -1,17 +1,17 @@
 resource "aws_s3_bucket" "dl" {
-    bucket = "datalake-logan-igti-edc-tf"
-    acl = "private"
+  bucket = "datalake-logan-igti-edc-tf"
+  acl    = "private"
 
-    tags = {
-        IES = "IGTI"
-        CURSO = "EDC"
-    }
+  tags = {
+    IES   = "IGTI"
+    CURSO = "EDC"
+  }
 
-    server_side_encryption_configuration {
-      rule {
-          apply_server_side_encryption_by_default {
-              sse_algorithm = "AES256"
-          }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
       }
     }
+  }
 }
