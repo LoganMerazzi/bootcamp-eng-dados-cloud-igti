a42cee23f8f7066a6767bb7848deb62cb46e5751
Logan D Merazzi <x503219@gruposantander.com>
2021-08-30 22:58:45 -0300

    Sobe infra airflow

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,3 +5,15 @@ variable "aws_region" {
 variable "lambda_function_name" {
   default = "IGTIexecutaEMRaovivo"
 }
+
+variable "key_pair_name" {
+  default = "keypair_logan_igti_edc"
+}
+
+variable "airflow_subnet_id" {
+  default = "subnet-97bf2bfc"
+}
+
+variable "vpc_id" {
+  default = "vpc-667e120d"
+}
